f154ff92c75cb05df6b1827cd7402e18f2aca43f
dbto <jens.lechner.dbto@googlemail.com>
2025-10-19 13:00:14 -0400

    Fix linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -16,7 +16,7 @@ sentiment_analyzer_url = os.getenv(
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
+    if (kwargs):
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
 
@@ -30,6 +30,7 @@ def get_request(endpoint, **kwargs):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
